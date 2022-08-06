d757adc2f0d105af6840a960022654ec75e813cd
Obaida616 <obaidaalhassan616@gmail.com>
2022-08-06 16:37:20 +0300

    adding logs to next app

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,9 @@ import axios from "axios";
 const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     // We are on the server
-
+    console.log('====================================');
+    console.log('Requesting on the server');
+    console.log('====================================');
     return axios.create({
       baseURL:
         "http://www.ez-app.online/",
@@ -11,6 +13,10 @@ const buildClient = ({ req }) => {
     });
   } else {
     // We must be on the browser
+    console.log('====================================');
+    console.log('Requesting on the browser');
+    console.log('====================================');
+
     return axios.create({
       baseUrl: "/",
     });
